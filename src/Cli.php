ac44986708646e636bde46765369442be3160a9d
Роман Гусев <r9045478349@gmail.com>
2025-12-26 22:18:56 +0500

    Add mod Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -3,8 +3,11 @@
 namespace BrainGames\Cli;
 
 use function cli\line;
+use function cli\prompt;
 
 function welcome(): void
 {
     line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
